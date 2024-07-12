e5b3b34afa9fac3e4a3561b8bb94bbcdfad24d67
able254 <muliable0110@gmail.com>
2024-07-12 11:18:09 +0300

    My first commit

---
+++ Files/home.php
@@ -0,0 +1,29 @@
+<?php
+    echo "Enter Your Message Here";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Simple Encryption Tool</title>
+</head>
+<body>
+    <h1>Encrypt Your Message</h1>
+    <!-- Encryption Message Box-->
+    <div class="container">
+        <textarea id="message" rows="5" placeholder="Enter your message here..."></textarea>
+        <button id="encryptButton">Encrypt</button>
+        <p id="encryptedMessage"></p>
+    </div>
+
+    <!-- Phone number Box -->
+    <div class="container">
+        <textarea id="message" rows="5" placeholder="Enter your message here..."></textarea>
+        <button id="encryptButton">Encrypt</button>
+        <p id="encryptedMessage"></p>
+    </div>
+
+    <!-- Implementation of The Algorithm-->
+</body>
+</html>
